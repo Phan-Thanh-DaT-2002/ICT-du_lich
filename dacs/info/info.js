808b907d59993f79111b0327e042416d11a319a0
Phan-Thanh-DaT-2002 <phandat2002h@gmail.com>
2022-11-29 00:27:25 +0700

    26

---
+++ dacs/info/info.js
@@ -0,0 +1,35 @@
+function y_10(){
+  if(document.getElementById('ngay_10').style.display === 'block'){
+    document.getElementById('ngay_10').style.display = 'none';
+  }
+  else  document.getElementById('ngay_10').style.display = 'block';
+}
+function y_12(){
+  if(document.getElementById('ngay_12').style.display === 'block'){
+    document.getElementById('ngay_12').style.display = 'none';
+  }
+  else  document.getElementById('ngay_12').style.display = 'block';
+
+}
+function y_18(){
+  if(document.getElementById('ngay_18').style.display === 'block'){
+    document.getElementById('ngay_18').style.display = 'none';
+  }
+  else  document.getElementById('ngay_18').style.display = 'block';
+}
+function y_25(){
+  if(document.getElementById('ngay_25').style.display === 'block'){
+    document.getElementById('ngay_25').style.display = 'none';
+  }
+  else  document.getElementById('ngay_25').style.display = 'block';
+}
+
+function t_t(){
+  if(document.getElementById('t_t').style.display === 'block'){
+    document.getElementById('t_t').style.display = 'none';
+  }
+  else  document.getElementById('t_t').style.display = 'block';
+}
+function set_time(){
+
+}
