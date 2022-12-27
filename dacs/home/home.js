7a8e22a7f37f6c1c148876bd6226348547841768
phandat00011469 <20010943@st.phenikaa-uni.edu.vn>
2022-12-28 01:49:44 +0700

    bao cao

--- dacs/home/home.js
+++ dacs/home/home.js
@@ -1,16 +1,20 @@
 function mu_cang_chai(){
   var link = document.querySelector('iframe');
+  link.classList.add('center');
   link.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29626.36111493384!2d104.0763827290708!3d21.846148654863917!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3132cfc5cc6e637b%3A0xde3403bcc89d81d5!2zdHQuIE3DuSBDYW5nIENo4bqjaSwgTcO5IENhbmcgQ2jhuqNpLCBZw6puIELDoWksIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1669542727831!5m2!1svi!2s';
 }
 function dien_bien_phu(){
   var link = document.querySelector('iframe');
+  link.classList.add('center');
   link.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59430.23676298337!2d103.0061504884164!3d21.413918412369906!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x312d085cec10365d%3A0x37a7fddbe27cbe64!2zVHAuIMSQaeG7h24gQmnDqm4gUGjhu6csIMSQaeG7h24gQmnDqm4sIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1669542955710!5m2!1svi!2s';
 }
 function moc_chau(){
   var link = document.querySelector('iframe');
+  link.classList.add('center');
   link.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29814.11947810177!2d104.73474641424067!3d20.921774857590222!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313393ffff215adb%3A0xccf7d9956d8fef68!2zQ2FvIG5ndXnDqm4gTeG7mWMgQ2jDonU!5e0!3m2!1svi!2s!4v1669543044062!5m2!1svi!2s';
 }
 function hang_tien_son(){
   var link = document.querySelector('iframe');
+  link.classList.add('center');
   link.src = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3690.9330565666346!2d103.65399371395957!3d22.31837138531416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x36cd5bf460b177af%3A0x2f10da3471050a93!2zxJDhu5luZyBUacOqbiBTxqFuLCBMYWkgQ2jDonU!5e0!3m2!1svi!2s!4v1669543084975!5m2!1svi!2s';
 }
